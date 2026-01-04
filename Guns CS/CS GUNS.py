cc5e7c750e7688ef30cb21b524c2222de6ad1f41
C0dy801 <santiagotorres8012@gmail.com>
2026-01-04 17:19:45 -0600

    Update CS GUNS.py

--- Guns CS/CS GUNS.py
+++ Guns CS/CS GUNS.py
@@ -7,6 +7,12 @@ py.init()
 screen = py.display.set_mode((450,450))
 py.display.set_caption('Choose Guns CS1.6')
 time = py.time.Clock()
+text = py.font.Font(None,20)
+text_2 = py.font.Font(None,20)
+text_3 = py.font.Font(None,20)
+text_surface = text.render(f"Presiona 0 para {config.tecla_1}",True,"white")
+text_surface_2 = text_2.render(f"Presiona 9 para {config.tecla_2}",True,"white")
+text_surface_3 = text_3.render(f"Presiona 8 para {config.tecla_3}",True,"white")
 running = True
 while running:
     for event in py.event.get():
@@ -14,6 +20,7 @@ while running:
             running = False
             sys.exit
 
+        
     lista = config.tecla_1
     lista_2 = config.tecla_2
     lista_3 = config.tecla_3
@@ -39,6 +46,9 @@ while running:
                 key.press_and_release(buy)
                 py.time.delay(160)
     screen.fill(('black'))
+    screen.blit(text_surface,(50,100))
+    screen.blit(text_surface_2,(50,200))
+    screen.blit(text_surface_3,(50,300))
     py.display.flip()
     time.tick(60)
 
